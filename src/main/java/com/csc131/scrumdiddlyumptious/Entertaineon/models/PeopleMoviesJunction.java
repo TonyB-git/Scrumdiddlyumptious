60f68c77a313cef4d605bc6b40d4053c19cf4017
reloueslaticsus <reloueslaticsus@users.noreply.github.com>
2020-11-24 18:53:36 -0800

    Added entities and data loader

---
+++ src/main/java/com/csc131/scrumdiddlyumptious/Entertaineon/models/PeopleMoviesJunction.java
@@ -0,0 +1,69 @@
+package com.csc131.scrumdiddlyumptious.Entertaineon.models;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "people_movies")
+public class PeopleMoviesJunction {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int people_movies_id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "person_id")
+	private Person person;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "movie_id")
+	private Movie movie;
+	
+	public PeopleMoviesJunction(Person person, Movie movie) {
+		this.person = person;
+		this.movie = movie;
+	}
+
+	public PeopleMoviesJunction() {
+		
+	}
+	
+	public int getPeople_movies_id() {
+		return people_movies_id;
+	}
+
+	public void setPeople_movies_id(int people_movies_id) {
+		this.people_movies_id = people_movies_id;
+	}
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+	@Override
+	public String toString() {
+		return "People_Movies [people_movies_id=" + people_movies_id + ", people=" + person + ", movies=" + movie
+				+ "]";
+	}
+	
+	
+	
+}
