8ad78fe472d74fb2ff23529d1e78d3a0ce4b865f
bryanbyrd <36066043+bryanbyrd@users.noreply.github.com>
2020-11-29 15:20:30 -0800

    Updated MainController
    Implemented the MySQL queries for the database that returns the
    specified movies

--- src/main/java/com/csc131/scrumdiddlyumptious/Entertaineon/controllers/MainController.java
+++ src/main/java/com/csc131/scrumdiddlyumptious/Entertaineon/controllers/MainController.java
@@ -9,10 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.csc131.scrumdiddlyumptious.Entertaineon.models.Movie;
+import com.csc131.scrumdiddlyumptious.Entertaineon.models.*;
 import com.csc131.scrumdiddlyumptious.Entertaineon.repository.UserRepository;
 
 @RestController
@@ -21,11 +22,37 @@ public class MainController {
 	
 	@Autowired
 	UserRepository userRepo;
+
+	@GetMapping(value = "/search")
+	public List<Movie> movieQuery(@RequestParam(value = "year", required = false) Integer year, @RequestParam(value = "award", required = false) String award){
+		return userRepo.getMovieByQuery(year, award);
+	}
+
+	@GetMapping(value = "movies/all")
+	@ResponseBody
+	public List<Movie> getAll(){
+		System.out.println("here");
+		return userRepo.getAll();
+	}
 	
 	@GetMapping(value = "movies/{id}")
 	public Optional<Movie> getPerson(@PathVariable int id){
 		return userRepo.findById(id);
 	}
 	
-//test
+	@GetMapping(value = "movies/title/{title}")
+	public Optional<Movie> getMovie(@PathVariable String title){
+		return userRepo.getMovieByTitle(title);
+	}
+	
+	@GetMapping(value = "movies/year/{year}")
+	public List<Movie> getMovieByYear(@PathVariable int year){
+		return userRepo.getMoviesByYear(year);
+	}
+	
+	@GetMapping(value = "movies/actor/{actor}")
+	public List<Movie> getMoviesByActor(@PathVariable String actor){
+		return userRepo.findByActor(actor);
+	}
+	
 } 	
