aff5eb01aa10c04817da7dc2ba71395b13cc4cfa
bryanbyrd <36066043+bryanbyrd@users.noreply.github.com>
2020-12-03 19:05:12 -0800

    Added @Repository annotation

--- src/main/java/com/csc131/scrumdiddlyumptious/Entertaineon/repository/PersonRepository.java
+++ src/main/java/com/csc131/scrumdiddlyumptious/Entertaineon/repository/PersonRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Integer>{
 	
 	@Query(value = "SELECT * FROM people", nativeQuery = true)
